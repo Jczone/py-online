8f9438efab5e21c6e4f976c45b4244c1f9b40a84
Jczone <windows_h_czj@163.com>
2023-07-20 10:26:40 +0800

    Update README

--- py_online/src/main/resources/static/user/2001/code/run.py
+++ py_online/src/main/resources/static/user/2001/code/run.py
@@ -3,6 +3,13 @@
 # please edit your code here:
 # code start
 
-print("hello world")
+#!/usr/bin/python
+ 
+count = 0
+while (count < 9):
+   print ('The count is:', count)
+   count = count + 1
+ 
+print ("Good bye!")
 
 # code end
